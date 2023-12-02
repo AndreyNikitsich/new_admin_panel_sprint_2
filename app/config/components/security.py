24baf53a82b28b2965d75c3e12944948081d61f3
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2023-12-02 18:35:40 +0300

    improve settings

--- app/config/components/security.py
+++ app/config/components/security.py
@@ -2,8 +2,8 @@ import os
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('|')
-INTERNAL_IPS = os.environ.get('INTERNAL_IPS').split('|')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split('|')
+INTERNAL_IPS = os.environ.get('INTERNAL_IPS', '').split('|')
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- app/config/settings.py
+++ app/config/settings.py
@@ -1,12 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
 from split_settings.tools import include
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DOTENV_PATH = os.path.join(BASE_DIR, '.env')
-load_dotenv(DOTENV_PATH)
 
 ROOT_URLCONF = 'config.urls'
 WSGI_APPLICATION = 'config.wsgi.application'
