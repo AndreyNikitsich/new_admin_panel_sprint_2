c8f5d5f31a97a5b6b4ac32af6a27e84d6f9fd1ae
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2023-12-04 21:25:34 +0300

    improve docker-compose up and down

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,4 +8,12 @@ then
         --username $DJANGO_SUPERUSER_USERNAME \
         --email $DJANGO_SUPERUSER_EMAIL || true
 fi
-uwsgi --strict --ini /opt/app/uwsgi.ini
+
+# use exec to handle SIGINT to speed up the container shutdown process
+if [ "$ENVIRONMENT" = "PROD" ]; then
+    exec uwsgi --strict --ini /opt/app/uwsgi.ini
+elif [ "$ENVIRONMENT" = "LOCAL" ]; then
+    exec python manage.py runserver 0.0.0.0:8000
+else
+    echo You are required to specify the ENVIRONMENT
+fi
